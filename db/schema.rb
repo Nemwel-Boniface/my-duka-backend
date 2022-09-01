d3595379f2e97dca5aba074f76482db62c34607f
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-09-01 08:54:06 +0300

    Update included:   - Generated migration to create the carts table   - Added refrences to the users and products table   - Geberated an updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_01_041917) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_01_055024) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "product_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_carts_on_product_id"
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "image"
@@ -57,5 +66,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_09_01_041917) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
+  add_foreign_key "carts", "products"
+  add_foreign_key "carts", "users"
   add_foreign_key "products", "users"
 end
