8491421d27d787d7cc321c162f359328696bce81
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-09-01 07:49:26 +0300

    Update included the enums for the products columns

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+  enum :status, {active: 0, archived: 1}
+  enum :type, {shoe_wears: 0, bags: 1, clothing: 2}
+  enum :color, {black: "#000", navy_blue: "#000080", red: "#FF0000", brown: "#964B00."}
 end
