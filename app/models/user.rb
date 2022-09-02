7ba2fe6f4bca78b1deb9620f6cf65b84ecb3b2c5
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-09-02 09:40:13 +0300

    Update included:   - Update to the users email validation   - Checked and fixed all found Rubocop offences

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,13 +6,14 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
 
-  enum :role, { buyer: 0, seller: 1, admin: 2}
+  enum :role, { buyer: 0, seller: 1, admin: 2 }
 
   validates :name, presence: true, length: { minimum: 3, maximum: 50 }
   validates :image, presence: true
-  validates :nickname, presence: true, length: { minimum: 3, maximum: 50}, allow_blank: true
+  validates :nickname, presence: true, length: { minimum: 3, maximum: 50 }, allow_blank: true
   validates :phone, presence: true, length: { minimum: 3, maximum: 50 }
-  validates :email, presence: true
+  validates :email, presence: true, uniqueness: true,
+                    format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: 'Invalid email format' }
   validates :location, presence: true, length: { minimum: 3, maximum: 50 }
 
   has_many :products
