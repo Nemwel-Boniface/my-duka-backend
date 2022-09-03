9525fe847dd28fbb5650d66d0a5f750ea4690636
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-09-03 22:11:11 +0300

    Update created endpoints to create a product

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::ProductsController < ApplicationController
+  # before_action :authenticate_user!
   def index
     @products = Product.all.where(status: 0)
     
@@ -7,5 +8,21 @@ class Api::V1::ProductsController < ApplicationController
     else
       render json: @products.errors, status: :bad_request
     end
+  end
+
+  def create
+    @product = Product.new(products_params)
+
+    if @product.save
+      render json: {status: "SUCCESS", message: "Product was created succesfully", data: @product}, status: :created
+    else
+      render json: @product.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def products_params
+    params.require(:product).permit(:image, :name, :description, :status, :price, :quantity, :product_type, :color, :user_id)
   end
 end
