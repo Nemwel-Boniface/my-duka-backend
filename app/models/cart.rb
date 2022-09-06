364f09f0963c21d8c9dd043f3fbc47b5fe82a96d
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2022-09-06 08:04:59 +0300

    Update included methods that:   - Increment the number of booked products   - Gets the totals of the booked products

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -4,4 +4,13 @@ class Cart < ApplicationRecord
 
   belongs_to :user
   has_many :products
+
+  def increment_product_count
+    @product.increment!(:product_count)
+  end
+
+  def get_total
+    @product = Product.find(params[:id])
+    product_total = product_count * price
+  end
 end
